30211f67859ba4f769983b283e88c916cf55555d
Emanoel Rainey <eraineycm8@gmail.com>
2023-04-27 23:25:32 -0300

    Teste xml

--- exemplo_php_mysql/protected.php
+++ exemplo_php_mysql/protected.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION)){
 }
 
 if(!isset($_SESSION['id'])){
-    header("Location: http://localhost/exemple/teste_login/index.php");
-    die('<h1>Você não pode acessar essa parte do site sem estar logado!</h1><br><p><a href=" http://localhost/exemple/teste_login/index.php">Entrar</a></p>');
+    header("Location: http://localhost/exemple/exemplo_php_mysql/index.php");
+    die('<h1>Você não pode acessar essa parte do site sem estar logado!</h1><br><p><a href=" http://localhost/exemple/exemplo_php_mysql/index.php">Entrar</a></p>');
 }
 ?>
---
+++ exemplo_xml/conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$usuario = 'root';
+$senha = '05120dcm';
+$database = 'login';
+$host = 'localhost';
+$mysqli = new mysqli($host,$usuario,$senha,$database);
+
+if($mysqli->error){
+    die("Falha ao conectar ao banco de dados");
+}
+/*else{
+    echo "OPAAAAAAAAAAAAA";
+}*/
+
+?>
